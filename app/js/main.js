bbd4d3f25f874566951017ffefc7cf7c35844449
Aleksandr <krasa19920906@gmail.com>
2023-01-03 19:39:17 +0300

    create section surf

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+
 $(".header__slider").slick({
     infinite:true,
     fade:true,
@@ -10,4 +11,21 @@ $(".slider-dots").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor:".header__slider"
+});
+
+$(".surf-slider").slick({
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/header/arrows-left.svg">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/header/arrows-right.svg">',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".surf-slider-map"
+});
+
+
+$(".surf-slider-map").slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    asNavFor: ".surf-slider",
+    arrows: false,
+    focusOnSelect:true
 });
